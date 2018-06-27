903443733e7c8499877d8af018a610f524ef0161
jessica schmitz and julia sheremet <jessica+julia@>
2018-06-27 15:57:20 -0700

    add images as variables and pass them into the shoe data to populate on the page

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'; 
+import {Switch, Route} from 'react-router-dom';
 
 import ShoeList from './ShoeList';
 import AboutUs from './AboutUs';
@@ -12,7 +12,7 @@ function Router(){
         <Route path='/aboutUs' component={AboutUs} />
       </Switch>
     </div>
-  )
+  );
 }
 
 export default Router;
--- src/components/ShoeList.jsx
+++ src/components/ShoeList.jsx
@@ -1,64 +1,66 @@
 import React from 'react';
 import banner from '../assets/img/banner.png';
-
+import ShoeItem from './ShoeItem';
+import redHeel from '../assets/img/redHeel.webp';
+import blueHeel from '../assets/img/blueHeel.webp';
 
 const shoeData = [
   {
-    image: '../assets/img/redHeel.webp',
-    title: 'The Day Heel',
-    color: 'Bright Red',
-    price: '$145'
-  },
-    image: '../assets/img/blueHeel.webp',
-    title: 'The Night Heel',
-    color: 'Bright Red',
-    price: '$200'
-  },
-    image: '../assets/img/redHeel.webp',
+    image: redHeel,
     title: 'The Day Heel',
     color: 'Bright Red',
     price: '$145'
   },
   {
-    image: '../assets/img/blueHeel.webp',
-    title: 'The Day Heel',
-    color: 'Bright Red',
-    price: '$145'
-  },
-    image: '../assets/img/redHeel.webp',
+    image: blueHeel,
     title: 'The Night Heel',
     color: 'Bright Red',
     price: '$200'
   },
-    image: '../assets/img/blueHeel.webp',
+  {
+    image: redHeel,
+    title: 'The Day Heel',
+    color: 'Bright Red',
+    price: '$145'
+  },
+  {
+    image: blueHeel,
+    title: 'The Day Heel',
+    color: 'Bright Red',
+    price: '$145'
+  },
+  {
+    image: redHeel,
+    title: 'The Night Heel',
+    color: 'Bright Red',
+    price: '$200'
+  },
+  {
+    image: blueHeel,
     title: 'Another Heel',
     color: 'Bright Red',
     price: '$145'
   },
   {
-    image: '../assets/img/redHeel.webp',
+    image: redHeel,
     title: 'The Day Heel',
     color: 'Bright Red',
     price: '$145'
   },
-    image: '../assets/img/blueHeel.webp',
+  {
+    image: blueHeel,
     title: 'Heels Galour',
     color: 'Bright Red',
     price: '$200'
-  },
-    image: '../assets/img/redHeel.webp',
-    title: 'Shoes',
-    color: 'Bright Red',
-    price: '$145'
-  },
-]
+  }
+];
 
 function ShoeList(){
   return(
     <div>
       <img src={banner} />
       <div className="ShoeData">
-        {shoeItem.map((shoe, index) =>
+        {shoeData.map((shoe, index) =>
           <ShoeItem
             image={shoe.image}
             title={shoe.title}
